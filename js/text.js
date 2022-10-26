04fda0c339b59194829ee8da2da827ba100d020f
taehunlim <dsa1520@naver.com>
2022-10-26 21:27:09 +0900

    Refactor: TextGeometry

--- js/text.js
+++ js/text.js
@@ -5,19 +5,26 @@ import { FontLoader } from "/js/three/FontLoader.js";
 
 const fontPath = "/js/three/fonts/";
 
-let fontLoader = new FontLoader();
+const VIEW_ANGLE = 75; // 카메라 시야각. 커질 수록 시야각이 넓어짐. 단위는 degree
+const ASPECT = window.innerWidth / window.innerHeight; //시야의 가로세로비
+const NEAR = 0.1; // 렌더링 할 물체 거리의 하한값. 너무 가까이 있는 물체를 그리는 것을 막기 위해 사용. 카메라로부터의 거리가 이 값보다 작은 물체는 화면에 그리지 않음. 0보다 크고 FAR 보다 작은 값을 가질 수 있다.
+const FAR = 1000; // 렌더링 할 물체 거리의 상한값. 너무 멀리 있는 물체를 그리는 것을 막기위해 사용. 카메라로부터의 거리가 이 값보다 큰 물체는 화면에 그리지 않는다.
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+const renderer = new THREE.WebGLRenderer();
+// 특정한 질감, 색, 반사율 등을 갖는 물체의 표면
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+
+camera.position.z = 20;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const fontLoader = new FontLoader();
 fontLoader.load(fontPath + "gentilis_bold.typeface.json", (font) => {
-  const VIEW_ANGLE = 75; // 카메라 시야각. 커질 수록 시야각이 넓어짐. 단위는 degree
-  const ASPECT = window.innerWidth / window.innerHeight; //시야의 가로세로비
-  const NEAR = 0.1; // 렌더링 할 물체 거리의 하한값. 너무 가까이 있는 물체를 그리는 것을 막기 위해 사용. 카메라로부터의 거리가 이 값보다 작은 물체는 화면에 그리지 않음. 0보다 크고 FAR 보다 작은 값을 가질 수 있다.
-  const FAR = 1000; // 렌더링 할 물체 거리의 상한값. 너무 멀리 있는 물체를 그리는 것을 막기위해 사용. 카메라로부터의 거리가 이 값보다 큰 물체는 화면에 그리지 않는다.
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
   // 뼈대
   const geometry = new TextGeometry("text", {
     font: font, // fontLoader를 통해 얻어온 폰트 객체
@@ -30,12 +37,6 @@ fontLoader.load(fontPath + "gentilis_bold.typeface.json", (font) => {
   });
   geometry.computeBoundingBox();
 
-  // 특정한 질감, 색, 반사율 등을 갖는 물체의 표면
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true,
-  });
-
   // Mesh = Geometry 에 material 이 입혀진 오브젝트 = 물체
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
@@ -43,8 +44,6 @@ fontLoader.load(fontPath + "gentilis_bold.typeface.json", (font) => {
   const centerOffset =
     -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
   mesh.position.x = centerOffset;
-
-  camera.position.z = 20;
 
   function animate() {
     requestAnimationFrame(animate);
