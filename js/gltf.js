3ce10195c6a3f6be36a19d10c01aa4eaad8014bc
taehunlim <dsa1520@naver.com>
2022-10-26 15:58:52 +0900

    Feat: GLTFLoader

---
+++ js/gltf.js
@@ -0,0 +1,36 @@
+import * as THREE from "/js/three/three.module.js";
+import { GLTFLoader } from "/js/three/GLTFLoader.js";
+
+const VIEW_ANGLE = 75;
+const ASPECT = window.innerWidth / window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#canvas"),
+});
+const light = new THREE.DirectionalLight(0xffff00, 10);
+scene.add(light);
+scene.background = new THREE.Color("white");
+
+camera.position.set(0, 0, 5);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const loader = new GLTFLoader();
+loader.load("/assets/images/3d/car/scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+  renderer.render(scene, camera);
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    const speed = Math.random() / 20;
+    gltf.scene.rotation.x += speed;
+    gltf.scene.rotation.y += speed;
+    gltf.scene.rotation.z += speed;
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+});
